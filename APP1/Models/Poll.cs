ddeacb65dd9afdc396408d291a3d6d0132e3725a
Momo60 <32074805+Momo60@users.noreply.github.com>
2017-09-21 15:53:28 -0400

    Ajout des fichiers de sondage
    Ajout des fichiers dans le dossier Models

--- APP1/Models/Poll.cs
+++ APP1/Models/Poll.cs
@@ -6,22 +6,22 @@ using System.Runtime.Serialization;
 
 namespace USherbrooke.ServiceModel.Sondage
 {
-	// <summary>
-	// Cette classe définit un sondage.
-	// </summary>
-	[DataContract]
-	public class Poll
-	{
-		// <summary>
-		// L'identifiant du sondage.
-		// </summary>
-		[DataMember]
-		public int Id { get; set; }
+    /// <summary>
+    /// Cette classe définit un sondage.
+    /// </summary>
+    [DataContract]
+    public class Poll
+    {
+        /// <summary>
+        /// L'identifiant du sondage.
+        /// </summary>
+        [DataMember]
+        public int Id { get; set; }
 
-		// <summary>
-		// La description du sondage.
-		// </summary>
-		[DataMember]
-		public string Description { get; set; }
-	}
+        /// <summary>
+        /// La description du sondage.
+        /// </summary>
+        [DataMember]
+        public string Description { get; set; }
+    }
 }
