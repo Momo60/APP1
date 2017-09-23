658b56c1910830f510f66c5ab6f6be9ba53f9c94
antoneys <stephane.antoney@hotmail.fr>
2017-09-23 00:59:37 -0400

    Test Sondage
    Rassemblage de l'authentification dans un token
    
    Test concernat l'affichage du token

---
+++ APP1/Controllers/SondageController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using APP1.Models;
+using Microsoft.AspNetCore.Mvc;
+using USherbrooke.ServiceModel.Sondage;
+
+namespace APP1.Controllers
+{
+	[Route("api/[controller]")]
+	public class SondageController : Controller
+	{
+/*		private readonly SondageContext _context;
+
+		public SondageController(SondageContext context)
+		{
+			_context = context;
+
+            if (_context.SimpleSondageDAO.Count() == 0)
+            {
+                _context.SimpleSondageDAO.Add(new USherbrooke.ServiceModel.Sondage.SimpleSondageDAO{});
+                _context.SaveChanges();
+            }
+		}
+*/
+        [HttpGet]
+        public IEnumerable<Poll> GetAll()
+        {
+            /*return new Poll[] {ISondageDAO. };*/
+            //return "Bonjour";
+            /*  return new ObjectResult(SimpleSondageDAO.GetAvailablePolls()); */
+            /*return Json(SimpleSondageDAO.GetAvailablePolls());*/
+
+
+            return Json(
+                SimpleSondageDAO.GetAvailablePolls();
+            );
+        }
+
+    }
+}
+     
--- APP1/Controllers/TokenController.cs
+++ APP1/Controllers/TokenController.cs
@@ -1,17 +1,22 @@
-﻿/***************************
-* Controller pour le token *
-****************************/
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using APP1;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+
 namespace APP1.Controllers
 {
-    [Route("api/[controller]")]
+	/***************************
+    * Controller pour le token *
+    ****************************/
+	[Route("api/[controller]")]
     public class TokenController : Controller
     {
         /*Envoi d'un nom en tant que Username*/
@@ -19,7 +24,30 @@ namespace APP1.Controllers
         public IActionResult Post(string userId)
 		{
             /*Génération et Renvoie du token*/
-            return new ObjectResult(AuthUser.GenerateToken(userId));
+            return new ObjectResult(GenerateToken(userId));
+		}
+
+		/**************************************
+        * Méthode pour la génération du token *
+        ***************************************/
+		public static string GenerateToken(string userId)
+		{
+			// secretKey contains a secret passphrase only your server knows
+			var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("stephane_mohammed"));
+
+			var claims = new Claim[] {
+				new Claim(ClaimTypes.Name, userId),
+				new Claim(JwtRegisteredClaimNames.Email, userId+"@usherbrooke.ca"),
+			new Claim(JwtRegisteredClaimNames.Exp, $"{new DateTimeOffset(DateTime.Now.AddDays(1)).ToUnixTimeSeconds()} "),
+			new Claim(JwtRegisteredClaimNames.Nbf, $"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()} ")
+			};
+
+			var token = new JwtSecurityToken(new JwtHeader(new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256)), new JwtPayload(claims));
+
+			string jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+			return jwtToken; /*retour du token*/
+
 		}
 
     }
